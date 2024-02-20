2dba9074801650b91a13e2ad68b137532583dff5
igopereira1 <igopereira1@gmail.com>
2024-02-20 00:33:40 -0300

    chore: update app.js to include express.json() middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const router = require('./routes/contactRoutes');
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 app.use('/api/contacts', router);
 
 module.exports = app;
