8d6afa77a5144a0c43bd68c4279663419d801dc6
igopereira1 <igopereira1@gmail.com>
2024-02-20 22:32:11 -0300

    chore: add error handling middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./routes/contactRoutes');
+const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/contacts', router);
+app.use(errorHandler);
 
 module.exports = app;
