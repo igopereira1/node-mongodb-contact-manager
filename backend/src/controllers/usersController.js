562f172499c766a43f58fbd76bb08061dd6195b6
igopereira1 <igopereira1@gmail.com>
2024-02-24 00:55:29 -0300

    feat: add loginUser function to controller

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
-const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 const registerUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -14,9 +15,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('User already registered');
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(`hashedPassword: ${hashedPassword}`);
     const user = await User.create({ username, email, password: hashedPassword });
-    console.log(`User created: ${user}`);
     if (user) {
         res.status(201).json(user);
     } else {
@@ -26,9 +25,34 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error('Missing required fields for login');
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            
+            {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+            },
+            
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+        console.log(`accessToken: ${accessToken}`);
+        res.json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 const profileUser = asyncHandler(async (req, res) => {
+    res.json({ message: 'User profile' });
 });
 
 module.exports = {
